75d514628368d9144506f2de192e11e09c70a1e4
maxidragon <maksymilian.gala1@gmail.com>
2024-02-02 20:14:38 +0100

    Fix link issue

--- frontend/src/Components/CompetitionList.tsx
+++ frontend/src/Components/CompetitionList.tsx
@@ -1,4 +1,4 @@
-import { List, ListItemButton, Grid, ListItemText, Link } from "@mui/material";
+import { List, ListItemButton, Grid, ListItemText, Link, ListItem } from "@mui/material";
 import { Competition } from "../logic/interfaces";
 import CompetitionEventsList from "./CompetitionEventsList";
 import { t } from "i18next";
@@ -7,50 +7,92 @@ const CompetitionList = ({ competitions, isPast }: { competitions: Competition[]
     return (
         <List dense={true} disablePadding>
             {competitions.map((competition) => (
-                <ListItemButton
-                    key={competition.id}
-                    component={Link}
-                    target="_blank"
-                    href={
-                        competition.wcaWebsite
-                            ? competition.wcaWebsite
-                            : "/"
-                    }
-                    sx={{
-                        marginTop: "1.5em",
-                    }}
-                >
-                    <Grid
-                        container
-                        direction="column"
-                        justifyContent="space-between"
-                        gap={1}
-                    >
-                        <Grid item>
-                            <ListItemText primary={competition.name} sx={{
-                                "& .MuiTypography-root": {
-                                    fontSize: "1.5em",
-                                }
-                            }} />
-                        </Grid>
-                        <Grid item>
-                            {t('date')}: {new Date(competition.startDate).toLocaleDateString()}
-                        </Grid>
-                        {(competition.wcaWebsite && !isPast) && (
-                            <>
+                <>
+                    {competition.wcaWebsite ? (
+                        <ListItemButton
+                            key={competition.id}
+                            component={Link}
+                            target="_blank"
+                            href={
+                                competition.wcaWebsite
+                                    ? competition.wcaWebsite
+                                    : "/"
+                            }
+                            sx={{
+                                marginTop: "1.5em",
+                            }}
+                        >
+                            <Grid
+                                container
+                                direction="column"
+                                justifyContent="space-between"
+                                gap={1}
+                            >
                                 <Grid item>
-                                    {t('registrationStarts')}: {new Date(competition.registrationOpen).toLocaleDateString()}
+                                    <ListItemText primary={competition.name} sx={{
+                                        "& .MuiTypography-root": {
+                                            fontSize: "1.5em",
+                                        }
+                                    }} />
                                 </Grid>
                                 <Grid item>
-                                    {t('registrationCloses')}:  {new Date(competition.registrationClose).toLocaleDateString()}
+                                    {t('date')}: {new Date(competition.startDate).toLocaleDateString()}
                                 </Grid>
-                            </>
-                        )}
-                        <Grid item>
-                            <CompetitionEventsList events={competition.events} />
-                        </Grid>
-                    </Grid>
-                </ListItemButton>
+                                {(competition.wcaWebsite && !isPast) && (
+                                    <>
+                                        <Grid item>
+                                            {t('registrationStarts')}: {new Date(competition.registrationOpen).toLocaleDateString()}
+                                        </Grid>
+                                        <Grid item>
+                                            {t('registrationCloses')}:  {new Date(competition.registrationClose).toLocaleDateString()}
+                                        </Grid>
+                                    </>
+                                )}
+                                <Grid item>
+                                    <CompetitionEventsList events={competition.events} />
+                                </Grid>
+                            </Grid>
+                        </ListItemButton>
+                    ) : (
+                        <ListItem
+                            key={competition.id}
+                            sx={{
+                                marginTop: "1.5em",
+                            }}
+                        >
+                            <Grid
+                                container
+                                direction="column"
+                                justifyContent="space-between"
+                                gap={1}
+                            >
+                                <Grid item>
+                                    <ListItemText primary={competition.name} sx={{
+                                        "& .MuiTypography-root": {
+                                            fontSize: "1.5em",
+                                        }
+                                    }} />
+                                </Grid>
+                                <Grid item>
+                                    {t('date')}: {new Date(competition.startDate).toLocaleDateString()}
+                                </Grid>
+                                {(competition.wcaWebsite && !isPast) && (
+                                    <>
+                                        <Grid item>
+                                            {t('registrationStarts')}: {new Date(competition.registrationOpen).toLocaleDateString()}
+                                        </Grid>
+                                        <Grid item>
+                                            {t('registrationCloses')}:  {new Date(competition.registrationClose).toLocaleDateString()}
+                                        </Grid>
+                                    </>
+                                )}
+                                <Grid item>
+                                    <CompetitionEventsList events={competition.events} />
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                    )}
+                </>
             ))}
         </List>
     );
